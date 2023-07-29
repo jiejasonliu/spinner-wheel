d23091934913d0e2f9705c067d03e7b17cb14493
jiejasonliu <jasonningliu@gmail.com>
2023-07-29 13:50:36 -0600

    fix(client): fix build failures by removing dead test code

--- client/src/models/wheel.ts
+++ client/src/models/wheel.ts
@@ -36,16 +36,3 @@ export function isValidRateOfEffect(rateOfEffect: string): boolean {
 export function isValidParticipantsList(commaDelimitedNames: string) {
   return /[a-zA-Z0-9\s]+(,[a-zA-Z0-9\s])*/.test(commaDelimitedNames);
 }
-
-export const TEST_WHEEL: Wheel = {
-  _id: "123",
-  title:
-    "Test wheel with three participants, rate-of-effect of 0.25, and last spun on July 1st, 2023.",
-  participants: [
-    { name: "Jason", weight: 0.291 },
-    { name: "Swifi", weight: 0.438 },
-    { name: "Hex", weight: 0.271 },
-  ],
-  rate_of_effect: 0.25,
-  last_spun_at: new Date("July 1, 2023"),
-};
