f0210b7a400cff3131fd0f766a262655f05bce14
jiejasonliu <jasonningliu@gmail.com>
2023-07-29 01:13:09 -0600

    feat(server): add cors middleware for relevant origins

--- server/app/api.py
+++ server/app/api.py
@@ -1,12 +1,30 @@
 from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .routers import wheels_router
 
 load_dotenv(find_dotenv())
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5173",
+        "https://127.0.0.1:5173",
+        "http://bingo-crew-spinner.vercel.app",
+        "https://bingo-crew-spinner.vercel.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # register any routers
 app.include_router(wheels_router.router)
+
 
 @app.get("/", tags=["Root"])
 async def hello():
